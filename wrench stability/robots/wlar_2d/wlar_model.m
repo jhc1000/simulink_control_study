f3dee12400d64a561ea1a37cbf08355db869e8a9
Chan <jhc1000@hanyang.ac.kr>
2024-12-13 23:43:41 +0900

    Update wlar_model.m

--- wrench stability/robots/wlar_2d/wlar_model.m
+++ wrench stability/robots/wlar_2d/wlar_model.m
@@ -7,10 +7,10 @@ classdef wlar_model
         % K = Knee 
         % W = wheel
 
-        LF_tau_lim = [96.0 150.0 120.0];  % HR, HP, K
-        LH_tau_lim = [96.0 150.0 120.0];  % HR, HP, K
-        RF_tau_lim = [96.0 150.0 120.0];  % HR, HP, K
-        RH_tau_lim = [96.0 150.0 120.0];  % HR, HP, K
+        LF_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
+        LH_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
+        RF_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
+        RH_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
 
     end
     methods
@@ -27,8 +27,8 @@ classdef wlar_model
             self.RF_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
             self.RH_tau_lim = [120.0 150.0 120.0];  % HR, HP, K
 
-            self.ASC_L_tension_lim = [75.0 400.0];  % min, max 
-            self.ASC_R_tension_lim = [75.0 400.0];  % min, max
+            self.ASC_L_tension_lim = [82.0 400.0];  % min, max 
+            self.ASC_R_tension_lim = [82.0 400.0];  % min, max
 
 
             self.ASC_L_tau_lim = 191.0; % Nm
